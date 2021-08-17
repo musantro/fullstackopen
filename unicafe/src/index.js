4b68019107095b3e070fb7c3ed107a3a0e233cd6
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2021-08-17 17:22:57 +0200

    feat: exercise 1.7

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -15,6 +15,7 @@ const App = () => {
 
     const handleClick = (set, inc) => () => set(inc + 1)
 
+    let all = good + neutral + bad;
     return (
         <div>
             <h1>give feedback</h1>
@@ -25,6 +26,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {(good - bad) / all}</p>
+            <p>positive {good/all * 100} %</p>
         </div>
     )
 }
