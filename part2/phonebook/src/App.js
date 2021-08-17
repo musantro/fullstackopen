c5b548b9f0cc2f57c279d1e34523134761ed5b23
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2021-08-18 01:49:59 +0200

    feat: exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,12 @@ import React, {useState} from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', id: 1}
+    {name: 'Arto Hellas', number: '911111111', id: 1}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [queryName, setQueryName] = useState('')
+
   const addName = (event) => {
     event.preventDefault()
     const names = new Set(persons.map(p => p.name))
@@ -15,30 +18,41 @@ const App = () => {
     else {
       const nameObject = {
         name: newName,
-        id: persons.length + 1
+        number: newNumber,
+        id: persons.length + 1,
       }
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
+  const handlePhoneChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleQueryChange = (event) => {
+    setQueryName(event.target.value)
+  }
+
+  const personsToShow = persons.filter(p => p.name.toLowerCase().includes(queryName.toLowerCase()))
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>PhoneBook</h2>
+      <div>filter shown with <input value={queryName} onChange={handleQueryChange}/></div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNameChange}/></div>
+        <div>number:  <input value={newNumber} onChange={handlePhoneChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(p => <div key={p.id}>{p.name}</div>)}
+      {personsToShow.map(p => <div key={p.id}>{p.name} {p.number}</div>)}
     </div>
   )
 }
